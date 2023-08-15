c17e556effe7f9ec9705b1987f2e847a79500c75
Caden-H <cadenrharris@gmail.com>
2023-08-15 15:24:45 -0600

    Added some SUGGESTIONS

--- Entities/enemy.js
+++ Entities/enemy.js
@@ -1,6 +1,6 @@
 
 class Enemy extends Entity{
-    // If the constuctor is exactly the same as Entity, you don't need this
+      // If the constuctor is exactly the same as Entity, you don't need this
     // Only use it if you're going to add aditional parameters on top of the entity defaults
     // constructor(name, type, health, attackspeed, damage, armor, gold, regen, complexStats) {
     //     super(name, type, health, attackspeed, damage, armor, gold, regen, complexStats);
