8391cd2182b238f8091872836dd618008d03e70f
Holden-nn <122432337+Holden-nn@users.noreply.github.com>
2023-08-16 21:05:41 -0600

    Finished implementing all Grassland enemies.

--- Entities/enemies.js
+++ Entities/enemies.js
@@ -1,7 +1,7 @@
 let el;
 $(function(){
     el = {
-        // world one, d-0
+        // grassland zone, d-0
         'ladybug': new Enemy('lady bug', 'bug', 6, -10, 1, 0, 5, 0, 0),
         'ant': new Enemy('ant', 'bug', 4, 20, 1, 0, 6, 0),
         'cricket': new Enemy('cricket', 'bug', 4, 10, 1, 0, 5, 0, 0),
@@ -9,7 +9,7 @@ $(function(){
         'fly': new Enemy('fly', 'bug', 3, 35, 1, 0, 5, 0, 0),
         'worm': new Enemy('fly', 'bug', 11, -150, 2, 0, 6, 0, 0),
 
-        // world one, d-1
+        // grassland zone, d-1
         'caterpillar': new Enemy('caterpillar', 'bug', 12, 0, 1, 0, 7, 0, 0),
         'centipede': new Enemy('centipede', 'bug', 16, -30, 1, 0, 7, 0, 0),
         'flea': new Enemy('flea','bug',8, 0, 2, 0, 6, 0, 0),
@@ -17,7 +17,7 @@ $(function(){
         'dragonfly': new Enemy('dragon fly','bug',13, -40, 2, 0, 8, 0, 0),
         'moth': new Enemy('moth','bug',8, 40, 1, 0, 6, 0, 0),
 
-        // world one, d-2
+        // grassland zone, d-2
         'blackbeetle': new Enemy('black beetle', 'bug', 14, 0, 1, 1, 8, 0, 0),
         'bluebeetle': new Enemy('blue beetle', 'bug', 13, 5, 1, 1, 8, 0, 0),
         'redbeetle': new Enemy('red beetle', 'bug', 12, 10, 1, 1, 8, 0, 0),
@@ -25,7 +25,7 @@ $(function(){
         'yellowbeetle': new Enemy('yellow beetle', 'bug', 16, -10, 1, 1, 8, 0, 0),
         'brownbeetle': new Enemy('brown beetle', 'bug', 17, -15, 1, 1, 8, 0, 0),
 
-        // world one, d-3
+        // grassland zone, d-3
         'rabbit': new Enemy('rabbit', 'critter', 18, 20, 2, 0, 8, 0, 0),
         'bunny': new Enemy('bunny', 'critter', 14, 45, 2, 0, 6, 0, 0),
         'hare': new Enemy('hare', 'critter', 21, 0, 2, 0, 8, 0, 0),
@@ -33,15 +33,15 @@ $(function(){
         'guineapig': new Enemy('guinea pig', 'critter', 25, 0, 1, 0, 9, 0, 0),
         'pika': new Enemy('pika', 'critter', 15, 70, 1, 0, 8, 0, 0),
 
-        // world one, d-4
+        // grassland zone, d-4
         'mole': new Enemy('mole', 'critter', 20, -100, 5, 0, 8, 0, 0),
-        'prairiedog': new Enemy('prairie dog', 'critter', 20, -80, 4, 0, 8, 0, 0),
+        'vole': new Enemy('prairie dog', 'critter', 20, 45, 3, 0, 8, 0, 0),
         'gopher': new Enemy('gopher', 'critter', 20, -100, 3, 1, 8, 0, 0),
-        'badger': new Enemy('badger', 'critter', 20, -120, 4, 1, 8, 0, 0),
+        'groundsquirrel': new Enemy('ground squirrel', 'critter', 20, -120, 4, 1, 8, 0, 0),
         'molerat': new Enemy('mole-rat', 'critter', 20, -160, 5, 1, 8, 0, 0),
         'meerkat': new Enemy('meerkat', 'critter', 20, -70, 2, 0, 8, 1, 0),
 
-        // world one, d-5
+        // grassland zone, d-5
         'sparrow': new Enemy('sparrow', 'bird', 24, 0, 4, 1, 8, 0, 0, {'dodge':0.25}),
         'swallow': new Enemy('swallow', 'bird', 27, 20, 4, 1, 9, 0, 0, {'dodge':0.25}),
         'lark': new Enemy('lark', 'bird', 22, 25, 3, 2, 11, 0, 0, {'dodge':0.33}),
@@ -49,13 +49,29 @@ $(function(){
         'burrowowl': new Enemy('burrow owl', 'bird', 21, -120, 5, 1, 9, 1, 0, {'dodge':0.33}),
         'quail': new Enemy('burrow owl', 'bird', 30, -20, 5, 2, 12, 1, 0),
 
-        // world one, d-6
-        'sparrow': new Enemy('sparrow', 'bird', 24, 0, 4, 1, 8, 0, 0, {'dodge':0.25}),
-        'swallow': new Enemy('swallow', 'bird', 27, 20, 4, 1, 9, 0, 0, {'dodge':0.25}),
-        'lark': new Enemy('lark', 'bird', 22, 25, 3, 2, 11, 0, 0, {'dodge':0.33}),
-        'dove': new Enemy('dove', 'bird', 18, -15, 3, 1, 12, 0, 0, {'dodge':0.51}),
-        'burrowowl': new Enemy('burrow owl', 'bird', 21, -120, 5, 1, 9, 1, 0, {'dodge':0.33}),
-        'quail': new Enemy('burrow owl', 'bird', 30, -20, 5, 2, 12, 1, 0),
+        // grassland zone, d-6
+        'prairiedog': new Enemy('prairie dog', 'critter', 30, -80, 6, 1, 8, 1, 0),
+        'badger': new Enemy('badger', 'critter', 32, 20, 4, 2, 11, 0, 0),
+        'ferret': new Enemy('ferret', 'critter', 38, -30, 5, 2, 10, 0, 0),
+        'jackrabbit': new Enemy('jackrabbit', 'critter', 28, 25, 4, 1, 10, 1, 0, {'dodge':0.10}),
+        'skunk': new Enemy('skunk', 'critter', 29, -700, 20, 2, 12, 1, 0),
+        'weasel': new Enemy('weasel', 'critter', 26, 40, 5, 2, 10, 1, 0),
+
+        // grassland zone, d-7 
+        'deer': new Enemy('deer','hoofed',48,10,4,2,13,1,0,{"dodge":0.2}),
+        'antelope': new Enemy('antelope','hoofed',52,-10,5,1,13,1,0),
+        'impala': new Enemy('impala','hoofed',54,25,3,1,10,1,0),
+        'wildebeast': new Enemy('wildebeast','hoofed',65,-60, 7, 2,18,1,0),
+        'pronghorn': new Enemy('pronghorn','hoofed',50,20,4,1,10,2,0),
+        'gazelle': new Enemy('gazelle','hoofed',42,20,3,2,13,1,0,{"dodge":0.4}),
+
+        // grassland zone, d-8
+        'fox': new Enemy('fox','critter', 56, 25, 5, 2, 15, 1, 0),
+        'coyote': new Enemy('coyote', 'critter', 62, -33, 7, 0, 16, 3, 0),
+        'mongoose': new Enemy('mongoose', 'critter', 40, 15, 6, 0, 12, 2, 0),
+        'bobcat': new Enemy('bobcat', 'critter', 80, 10, 10, 2, 24, 1, -1),
+        'jackal': new Enemy('jackal', 'critter', 50, 10, 7, 1, 12, 1, 0, {'dodge':0.2}),
+        'hyena': new Enemy('hyena', 'critter', 70, 0, 9, 1, 20, 2, 0)
     }
 });
 
@@ -80,7 +96,7 @@ function pullEnemy(){
                     }else if(x<0.75){ // medium
                         exE = pullEFromSix([el.blackbeetle,el.bluebeetle,el.redbeetle,el.greenbeetle,el.yellowbeetle,el.brownbeetle],true);
                     }else{ // hard
-                        exE = pullEFromSix([el.rabbit.lowDi(),el.bunny.lowDi(),el.hare.lowDi(),el.bunnyrabbit.lowDi(),el.guineapig.lowDi(),el.pika].lowDi(),true);
+                        exE = pullEFromSix([el.rabbit.lowDi(),el.bunny.lowDi(),el.hare.lowDi(),el.bunnyrabbit.lowDi(),el.guineapig.lowDi(),el.pika.lowDi()],true);
                     }
                 break;
                 case 3:
@@ -89,78 +105,55 @@ function pullEnemy(){
                     }else if(x<0.75){ // medium
                         exE = pullEFromSix([el.rabbit,el.bunny,el.hare,el.bunnyrabbit,el.guineapig,el.pika],true);
                     }else{ // hard
-                        exE = pullEFromSix([el.mole.lowDi(),el.prairiedog.lowDi(),el.gopher.lowDi(),el.badger.lowDi(),el.molerat.lowDi(),el.meerkat.lowDi()],true);
+                        exE = pullEFromSix([el.mole.lowDi(),el.vole.lowDi(),el.gopher.lowDi(),el.groundsquirrel.lowDi(),el.molerat.lowDi(),el.meerkat],true);                    
                     }
                 break;
                 case 4:
                     if(x<0.30){ // easy
                         exE = pullEFromSix([el.rabbit.incDi(),el.bunny.incDi(),el.hare.incDi(),el.bunnyrabbit.incDi(),el.guineapig.incDi(),el.pika.incDi()],true);
                     }else if(x<0.75){ // medium
-                        exE = pullEFromSix([el.mole,el.prairiedog,el.gopher,el.badger,el.molerat,el.meerkat],true);                    
+                        exE = pullEFromSix([el.mole,el.vole,el.gopher,el.groundsquirrel,el.molerat,el.meerkat],true);                    
                     }else{ // hard
                         exE = pullEFromSix([el.sparrow.lowDi(),el.swallow.lowDi(),el.lark.lowDi(),el.dove.lowDi(),el.burrowowl.lowDi(),el.quail.lowDi()],true);
                     }
                 break;
                 case 5:
                     if(x<0.30){ // easy
-                        exE = pullEFromSix([el.mole.incDi(),el.prairiedog.incDi(),el.gopher.incDi(),el.badger.incDi(),el.molerat.incDi(),el.meerkat.incDi()],true);                    
+                        exE = pullEFromSix([el.mole.incDi(),el.vole.incDi(),el.gopher.incDi(),el.groundsquirrel.incDi(),el.molerat.incDi(),el.meerkat.incDi()],true);                    
                     }else if(x<0.75){ // medium
                         exE = pullEFromSix([el.sparrow,el.swallow,el.lark,el.dove,el.burrowowl,el.quail],true);
                     }else{ // hard
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
+                        exE = pullEFromSix([el.prairiedog.lowDi(),el.badger.lowDi(),el.ferret.lowDi(),el.jackrabbit.lowDi(),el.skunk.lowDi(),el.weasel.lowDi()],true);
                     }
                 break;
                 case 6:
                     if(x<0.30){ // easy
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
+                        exE = pullEFromSix([el.sparrow.incDi(),el.swallow.incDi(),el.lark.incDi(),el.dove.incDi(),el.burrowowl.incDi(),el.quail.incDi()],true);
                     }else if(x<0.75){ // medium
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
+                        exE = pullEFromSix([el.prairiedog,el.badger,el.ferret,el.jackrabbit,el.skunk,el.weasel],true);
                     }else{ // hard
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
+                        exE = pullEFromSix([el.deer.lowDi(),el.antelope.lowDi(),el.impala.lowDi(),el.wildebeast.lowDi(),el.pronghorn.lowDi(),el.gazelle.lowDi()],true);
                     }
                 break;
                 case 7:
                     if(x<0.30){ // easy
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
+                        exE = pullEFromSix([el.prairiedog.incDi(),el.badger.incDi(),el.ferret.incDi(),el.jackrabbit.incDi(),el.skunk.incDi(),el.weasel.incDi()],true);
                     }else if(x<0.75){ // medium
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
+                        exE = pullEFromSix([el.deer,el.antelope,el.impala,el.wildebeast,el.pronghorn,el.gazelle],true);
                     }else{ // hard
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
+                        exE = pullEFromSix([el.fox.lowDi(),el.coyote.lowDi(),el.mongoose.lowDi(),el.bobcat.lowDi(),el.jackal.lowDi(),el.hyena.lowDi()],true);
                     }
                 break;
                 case 8:
-                    if(x<0.30){ // easy
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
-                    }else if(x<0.75){ // medium
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
+                    if(x<0.25){ // medium
+                        exE = pullEFromSix([el.deer,el.antelope,el.impala,el.wildebeast,el.pronghorn,el.gazelle],true);
                     }else{ // hard
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
+                        exE = pullEFromSix([el.fox.lowDi(),el.coyote.lowDi(),el.mongoose.lowDi(),el.bobcat.lowDi(),el.jackal.lowDi(),el.hyena.lowDi()],true);
                     }
                 break;
                 case 9:
-                    if(x<0.30){ // easy
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
-                    }else if(x<0.75){ // medium
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
-                    }else{ // hard
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
-                    }
-                break;
-                case 10:
-                    if(x<0.30){ // easy
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
-                    }else if(x<0.75){ // medium
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
-                    }else{ // hard
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
-                    }
-                break;
-                case 11:
-                    if(x<0.65){ // medium
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
-                    }else{ // hard
-                        exE = pullEFromSix([el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug,el.ladybug],true);
-                    }
+                    // all hard
+                    exE = pullEFromSix([el.fox.lowDi(),el.coyote.lowDi(),el.mongoose.lowDi(),el.bobcat.lowDi(),el.jackal.lowDi(),el.hyena.lowDi()],true);
                 break;
             }
         break;
