c368b52ea7acd4b15df02af36b9b31822a6c4ed3
Grant Nelson <antgrgmn@gmail.com>
2023-08-22 14:00:05 -0600

    minor format changes and bug fixes

--- Zones/ZoneTypes/grasslands.js
+++ Zones/ZoneTypes/grasslands.js
@@ -2,7 +2,7 @@ class Grasslands extends Zone {
     constructor(zoneLevel = 1) {
         super(zoneLevel);
         this.maxZoneLevel = 9;
-        this.shopType = [3,2,3,1]; //shop gen [weaponNumber, armorNumber, statNumber, usableNumber]
+        this.shopCode = [3,2,3,1]; //shop gen [weaponNumber, armorNumber, statNumber, usableNumber]
         this.pathGen = [14, 0.50, 0.15, 0, 0.15, 0.09, 0, 0.05, 0.05, 0.05]; //path gen, shop start, shop grow, shop reset, event start, event grow, event reset, rest start, rest grow, rest reset
         this.zoneLable = 'grassland';
         //could also do this through classes if you wanted to add more complex behavior to individual enemies
@@ -84,10 +84,12 @@ class Grasslands extends Zone {
                 ['hyena', 'critter', 70, 0, 9, 1, 20, 2, 0]
             ]
         };
+
         this.bossStats = [
             ['hairy g.o.a.t.', 'critter', 115, -25, 13, 2, 75, 1, 0, {'tear':2}],
             ['big fat bison', 'hoofed', 140, -125, 17, 4, 80, 1, 0]
         ];
+
         //could assign this in the parent class if this distribution holds across zones
         this.levelDifficultyDist = {
             //zoneLevel : [easyPercent, mediumPercent] (hard is excluded because it is defaulted to)
