a91ec5a880bbff761476b1aa32a4308446a5f4e1
Holden-nn <122432337+Holden-nn@users.noreply.github.com>
2023-08-22 02:04:18 -0600

    More spell comments

--- Items/ItemTypes/magic.js
+++ Items/ItemTypes/magic.js
@@ -1,7 +1,10 @@
 // Purchased from shop
 // Each spell has an appropriate mana cost (and potential other costs including but not limited to hp, further types of mana, etc.)
-// Each spell has a cool down in addition to it's mana cost (around 1 second to deter spam clicking)
+// Each spell has a cool down in addition to its mana cost (around 1 second to deter spam clicking)
 // Each spell has a ability which functions similar to a usable item
+// Each spell has an associated button element which on click does the spell's ability after checking for costs
+// Some spells may have finite uses in addition to the above requirements.
+// Each spell has a short description placed on its button and a long description that can be obtained from hovering over.
 
 
 class Magic extends Item {
