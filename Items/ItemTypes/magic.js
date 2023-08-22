0ca67dd210d4f3982af121c1159a86d0280ec8b4
Holden-nn <122432337+Holden-nn@users.noreply.github.com>
2023-08-22 02:01:03 -0600

    Commented features of item

--- Items/ItemTypes/magic.js
+++ Items/ItemTypes/magic.js
@@ -1,3 +1,9 @@
+// Purchased from shop
+// Each spell has an appropriate mana cost (and potential other costs including but not limited to hp, further types of mana, etc.)
+// Each spell has a cool down in addition to it's mana cost (around 1 second to deter spam clicking)
+// Each spell has a ability which functions similar to a usable item
+
+
 class Magic extends Item {
     constructor(name, metatype, complexStats) {
         super(name, metatype);
