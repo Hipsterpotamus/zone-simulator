d73177832cc9e59042f684a1a1a267b04365aecc
Holden-nn <122432337+Holden-nn@users.noreply.github.com>
2023-08-22 02:43:26 -0600

    Changed characters on select screen and commented characteristic ideas

---
+++ characteristics.js
@@ -0,0 +1,14 @@
+//characteristic ideas
+
+
+// Persuasive: -15% reduction on all future shop prices
+// Mechanical: +50% uses on all useables
+// Vengeful: +30% dmg when below 30% health
+// Prescient: see the name of the next enemy before next combat (or see ahead four spaces or something)
+// Elusive: gains +5% dodge for evey 15 attack speed
+// Dominant: gains 25% more gold upon killing an enemy/boss in less than 30 seconds
+// Intimidating: enemy loses 20% of armor upon start of battle
+// Precise: enemies takes 50% more damage from items, and 25% more from spells. 
+// Patient: receives a 30% damage increase after 1 minute
+// Thrifty: gains +40% gold from income 
+
