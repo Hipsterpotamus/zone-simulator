99cc70559f409c4fbab664388f7d30179f0823ab
Caden <cadenrharris@gmail.com>
2023-08-27 14:43:09 -0600

    Added maxHP icon and changed style for button types

--- Items/item.js
+++ Items/item.js
@@ -47,20 +47,10 @@ class Item {
 
 
             // Stat icons on shop buttons
-        // add a div inside the button in this structure
-        // <div class="shop-item-stats">
-        //     <div class="stat-up"> <SVG HERE> </div>
-        //     <div class="arm-down"> <SVG HERE> </div>
-        // </div>
         let shopItemStats = $('<div>', {
             'class': 'shop-item-stats',
             'id': this.name.replace(/\s/g, '') + '-stats'
         });
-        
-        // add the stat up/down icon divs based on which stats are affected by the item
-        // [type, damage, armor, regen, attackSpeedChange, complexStats]
-
-        // for stat in stat_icons
         for (let stat in stat_icons) {
             if (this.itemInfo[stat] < 0) {
                 let statDown = $('<div>', {
@@ -76,23 +66,7 @@ class Item {
                 statUp.html(stat_icons[stat])
             }
         }
-        
-        // if (this.itemInfo[1] > 0) {
-        //     let armUp = $('<div>', {
-        //         'class': 'stat-up shop-stat-dmg'
-        //     });
-        //     armUp.appendTo(shopItemStats);
-        //     $('.shop-stat-dmg').html(stat_icons['dmg'])
-        // } else if (this.itemInfo[2] > 0) {
-        //     let armUp = $('<div>', {
-        //         'class': 'stat-up shop-stat-arm'
-        //     });
-        //     armUp.appendTo(shopItemStats);
-        //     $('.shop-stat-arm').html(stat_icons['arm'])
-        // }
-        // add to the button
         shopItemStats.appendTo(this.shopElement);
-
 
         this.shopElement.on('click', () => {
             this.purchase();
